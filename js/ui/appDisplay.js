5462a949bf0bea26e3c7147726c89aa81cdd21a7
Jonas Dreßler <verdre@v0yd.nl>
2022-04-09 19:13:28 +0200

    swipeTracker: Make it a subclass of ClutterPanGesture
    The new ClutterGesture subclass ClutterPanGesture fits the SwipeTracker
    quite well. API wise the biggest difference to the TouchSwipeGesture it
    was using before is that ClutterPanGesture has its own internal event
    history to keep track of deltas and to calculate a velocity in the end.
    That means we need to give it slighty special treatment right now, but
    we'll improve that with the next commits.
    
    So remove the internal TouchSwipeGesture and make the SwipeTracker a
    subclass of ClutterPanGesture, this allows for giving the individual
    SwipeTrackers proper debug-names and to have fine-grained control over
    whether they should listen to events using the capture or bubble phase.
    
    am bla

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -320,6 +320,8 @@ var BaseAppView = GObject.registerClass({
         this._swipeTracker.connect('begin', this._swipeBegin.bind(this));
         this._swipeTracker.connect('update', this._swipeUpdate.bind(this));
         this._swipeTracker.connect('end', this._swipeEnd.bind(this));
+        this._swipeTracker.set_name('AppDisplay swipe tracker');
+        this._scrollView.add_action(this._swipeTracker);
 
         this._availWidth = 0;
         this._availHeight = 0;
