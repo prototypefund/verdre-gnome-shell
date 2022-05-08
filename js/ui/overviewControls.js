19d9c4e060e5bf8fc69688dc27f9af3d1bb0f6bc
Jonas Dreßler <verdre@v0yd.nl>
2022-05-08 15:56:18 +0200

    overview: Replace prepareToEnterOverview() with vfunc_show()
    We always call this when we want to open the overview and thus show the
    workspacesView, so we can easily use vfunc_show() for this. Using that
    vfunc has the added benefit that it won't be executed twice when called
    multiple times.

--- js/ui/overviewControls.js
+++ js/ui/overviewControls.js
@@ -719,7 +719,7 @@ class ControlsManager extends St.Widget {
         this._ignoreShowAppsButtonToggle = true;
 
         this._searchController.prepareToEnterOverview();
-        this._workspacesDisplay.prepareToEnterOverview();
+        this._workspacesDisplay.show();
 
         this._stateAdjustment.value = ControlsState.HIDDEN;
         this._stateAdjustment.ease(state, {
@@ -775,7 +775,7 @@ class ControlsManager extends St.Widget {
         this._stateAdjustment.remove_transition('value');
 
         tracker.confirmSwipe(baseDistance, points, progress, cancelProgress);
-        this._workspacesDisplay.prepareToEnterOverview();
+        this._workspacesDisplay.show();
         this._searchController.prepareToEnterOverview();
         this._stateAdjustment.gestureInProgress = true;
     }
@@ -805,7 +805,7 @@ class ControlsManager extends St.Widget {
         this._ignoreShowAppsButtonToggle = true;
 
         this._searchController.prepareToEnterOverview();
-        this._workspacesDisplay.prepareToEnterOverview();
+        this._workspacesDisplay.show();
 
         this._stateAdjustment.value = ControlsState.HIDDEN;
         this._stateAdjustment.ease(ControlsState.WINDOW_PICKER, {
