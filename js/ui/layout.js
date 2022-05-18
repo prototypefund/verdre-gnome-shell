862f8d7d07bc299e449f6d379cac5646e1e7f316
Jonas Dreßler <verdre@v0yd.nl>
2022-05-18 15:19:27 +0200

    layout: Update workspace work areas immediately
    Since we now no longer do all the workspace and window changes inside
    an idle, it can happen that windows get moved to another workspace
    before the workspace has its appropriate workarea set. This will make
    the window too large.
    
    To avoid that, update the work areas of newly added workspaces
    immediately.

--- js/ui/layout.js
+++ js/ui/layout.js
@@ -304,7 +304,7 @@ var LayoutManager = GObject.registerClass({
         // Need to update struts on new workspaces when they are added
         let workspaceManager = global.workspace_manager;
         workspaceManager.connect('notify::n-workspaces',
-                                 this._queueUpdateRegions.bind(this));
+                                 this._updateRegions.bind(this));
 
         let display = global.display;
         display.connect('restacked',
