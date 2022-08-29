f06b07129dea44ea583fd206198cd472125e5de9
Jonas Dreßler <verdre@v0yd.nl>
2022-08-29 13:15:55 +0200

    keyboard: always open with layer 0

--- js/ui/keyboard.js
+++ js/ui/keyboard.js
@@ -2157,6 +2157,7 @@ log("KEYBOARD: using cheap keyval press");
         this._animateShow();
 
         this._setEmojiActive(false);
+        this._setActiveLayer(0);
 
         this._panGesture.enabled = true;
     }
