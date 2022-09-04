5f73243ee4069396ae2cbecf8d40ec5f9cb4658b
Jonas Dreßler <verdre@v0yd.nl>
2022-09-04 12:39:43 +0200

    messageTray: Auto-expand message banners if last input was a touchscreen
    On touchscreens we can't make use of cursor hover as we do on
    traditional desktops to expand a banner that offers an action. Instead
    we should always expand the banner on touchscreens to make sure the user
    sees the action.

--- js/ui/messageTray.js
+++ js/ui/messageTray.js
@@ -903,6 +903,16 @@ var MessageTray = GObject.registerClass({
                               Shell.ActionMode.OVERVIEW,
                               this._expandActiveNotification.bind(this));
 
+        this._lastDeviceIsTouchscreen = false;
+        global.backend.connect('last-device-changed', (backend, device) => {
+            if (device.device_type === Clutter.InputDeviceType.TOUCHSCREEN_DEVICE)
+                this._lastDeviceIsTouchscreen = true;
+            else if (device.device_type === Clutter.InputDeviceType.POINTER_DEVICE ||
+                     device.device_type === Clutter.InputDeviceType.TOUCHPAD_DEVICE ||
+                     device.device_type === Clutter.InputDeviceType.TABLET_DEVICE)
+                this._lastDeviceIsTouchscreen = false;
+        });
+
         this._sources = new Set();
 
         this._sessionUpdated();
@@ -1269,7 +1279,8 @@ var MessageTray = GObject.registerClass({
         // We auto-expand notifications with CRITICAL urgency, or for which the relevant setting
         // is on in the control center.
         if (this._notification.urgency == Urgency.CRITICAL ||
-            this._notification.source.policy.forceExpanded)
+            this._notification.source.policy.forceExpanded ||
+            this._lastDeviceIsTouchscreen)
             this._expandBanner(true);
 
         // We tween all notifications to full opacity. This ensures that both new notifications and
