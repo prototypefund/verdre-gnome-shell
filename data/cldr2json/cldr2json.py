aff30c25121e0dfb8ad2bf0e9f21ab7122d2fd26
Jonas Dreßler <verdre@v0yd.nl>
2022-11-27 15:42:02 +0100

    keyboard: Autogenerate mobile layouts
    autogen

--- data/cldr2json/cldr2json.py
+++ data/cldr2json/cldr2json.py
@@ -149,6 +149,125 @@ def locale_to_xkb(locale, name):
                 return xkb
     raise KeyError("failed to find XKB mapping for %s" % locale)
 
+def create_char_key(strings, keyval, label, iconName, width):
+    obj = {}
+
+    if (keyval is not None):
+        obj["keyval"] = keyval
+    if (label is not None):
+        obj["label"] = label
+    elif (strings is not None):
+        obj["strings"] = strings
+    if (iconName is not None):
+        obj["iconName"] = iconName
+    if (width is not None):
+        obj["width"] = width
+
+    return obj
+
+def create_action_key(action, level, label, iconName, width):
+    obj = { "action": action }
+
+    if (level is not None):
+        obj["level"] = level
+    if (label is not None):
+        obj["label"] = label
+    if (iconName is not None):
+        obj["iconName"] = iconName
+    if (width is not None):
+        obj["width"] = width
+
+    return obj
+
+def create_modifier_key(keyval, label, iconName, width):
+    obj = { "keyval": keyval, "action": "modifier" }
+
+    if (label is not None):
+        obj["label"] = label
+    if (iconName is not None):
+        obj["iconName"] = iconName
+    if (width is not None):
+        obj["width"] = width
+
+    return obj
+
+def find_layer(root, layer):
+    for l in root["levels"]:
+        if (l["level"] == layer):
+            return l;
+
+    return None;
+
+def create_mobile(root):
+    lowercase_level = find_layer(root, "")
+    uppercase_level = find_layer(root, "shift")
+    optkeys_level = find_layer(root, "opt")
+    optshiftkeys_level = find_layer(root, "opt+shift")
+
+    lowercase_level["rows"][2].insert(0, create_action_key("levelSwitch", 1, None, "keyboard-shift-symbolic", 1.5))
+    lowercase_level["rows"][2].append(create_action_key("delete", None, None, "edit-clear-symbolic", 1.5))
+
+    lowercase_level["rows"][3].insert(0, create_action_key("levelSwitch", 2, "123", None, 1.5))
+    lowercase_level["rows"][3].insert(1, create_action_key("emoji", None, None, "smile-symbolic", 1))
+    lowercase_level["rows"][3][3] = create_char_key([" "], None, None, None, 4)
+    lowercase_level["rows"][3].append(create_char_key(None, "0xff0d", None, "keyboard-enter-symbolic", 1.5))
+
+    if (uppercase_level):
+        uppercase_level["rows"][2].insert(0, create_action_key("levelSwitch", 0, None, "keyboard-shift-symbolic", 1.5))
+        uppercase_level["rows"][2].append(create_action_key("delete", None, None, "edit-clear-symbolic", 1.5))
+
+        uppercase_level["rows"][3].insert(0, create_action_key("levelSwitch", 2, "123", None, 1.5))
+        uppercase_level["rows"][3].insert(1, create_action_key("emoji", None, None, "smile-symbolic", 1))
+        uppercase_level["rows"][3][3] = create_char_key([" "], None, None, None, 4)
+        uppercase_level["rows"][3].append(create_char_key(None, "0xff0d", None, "keyboard-enter-symbolic", 1.5))
+
+    optkeys_level["rows"][2].insert(0, create_action_key("levelSwitch", 3, "=/<", None, 1.5))
+    optkeys_level["rows"][2].append(create_action_key("delete", None, None, "edit-clear-symbolic", 1.5))
+
+    # Remove ° so that - in opt can be _ in opt+shift
+    degree_key = optshiftkeys_level["rows"][1][5];
+    optshiftkeys_level["rows"][1][5] = optkeys_level["rows"][3].pop(0)
+
+    # Move / to last free entry in 2nd row
+    optkeys_level["rows"][1].append(optkeys_level["rows"][3].pop(0))
+
+    optkeys_level["rows"][3].insert(0, create_action_key("levelSwitch", 0, "ABC", None, 1.5))
+    optkeys_level["rows"][3].insert(1, create_action_key("emoji", None, None, "smile-symbolic", 1))
+    optkeys_level["rows"][3].pop(2)
+    optkeys_level["rows"][3].insert(3, create_char_key([" "], None, None, None, 4))
+    optkeys_level["rows"][3].append(create_char_key(None, "0xff0d", None, "keyboard-enter-symbolic", 1.5))
+
+    # Move \ to last free entry in 2nd row
+    optshiftkeys_level["rows"][1].append(optshiftkeys_level["rows"][2].pop(0))
+
+    optshiftkeys_level["rows"][2].insert(0, degree_key);
+    optshiftkeys_level["rows"][2].insert(0, create_action_key("levelSwitch", 2, "123", None, 1.5))
+    optshiftkeys_level["rows"][2].append(create_action_key("delete", None, None, "edit-clear-symbolic", 1.5))
+
+    optshiftkeys_level["rows"][3].pop(3)
+    optshiftkeys_level["rows"][3].pop(3)
+    optshiftkeys_level["rows"][3].insert(0, create_action_key("levelSwitch", 0, "ABC", None, 1.5))
+    optshiftkeys_level["rows"][3].insert(1, create_action_key("emoji", None, None, "smile-symbolic", 1))
+    optshiftkeys_level["rows"][3].pop(4)
+    optshiftkeys_level["rows"][3].insert(3, create_char_key([" "], None, None, None, 4))
+    optshiftkeys_level["rows"][3].append(create_char_key(None, "0xff0d", None, "keyboard-enter-symbolic", 1.5))
+
+    return root
+
+def create_mobile_terminal(mobile_root):
+    terminal_row = []
+    terminal_row.append(create_char_key(None, "0xff09", "Tab", None, 2))
+    terminal_row.append(create_modifier_key("0xffe3", "Ctrl", None, 2))
+    terminal_row.append(create_modifier_key("0xffe9", "Alt", None, 2))
+    terminal_row.append(create_char_key(None, "0xff51", None, "go-previous-symbolic", 1))
+    terminal_row.append(create_char_key(None, "0xff52", None, "go-up-symbolic", 1))
+    terminal_row.append(create_char_key(None, "0xff54", None, "go-down-symbolic", 1))
+    terminal_row.append(create_char_key(None, "0xff53", None, "go-next-symbolic", 1))
+
+    for level in mobile_root["levels"]:
+        level["rows"].insert(0, terminal_row)
+
+    return mobile_root
 
 def convert_file(source_file, destination_path):
     logging.info("Parsing %s", source_file)
@@ -168,6 +287,40 @@ def convert_file(source_file, destination_path):
     try:
         with open(destination_file, 'x', encoding="utf-8") as dest_fd:
             json.dump(root, dest_fd, ensure_ascii=False, indent=2, sort_keys=True)
+    except FileExistsError as e:
+        logging.info("File %s exists, not updating", destination_file)
+
+    logging.debug("written %s", destination_file)
+
+    mobile_root = create_mobile(root)
+
+    try:
+        xkb_name = locale_to_xkb(root["locale"], root["name"])
+    except KeyError as e:
+        logging.warning(e)
+        return False
+    destination_file = os.path.join(destination_path, xkb_name + "-mobile.json")
+
+    try:
+        with open(destination_file, 'x', encoding="utf-8") as dest_fd:
+            json.dump(mobile_root, dest_fd, ensure_ascii=False, indent=2, sort_keys=True)
+    except FileExistsError as e:
+        logging.info("File %s exists, not updating", destination_file)
+
+    logging.debug("written %s", destination_file)
+
+    mobile_terminal_root = create_mobile_terminal(mobile_root)
+
+    try:
+        xkb_name = locale_to_xkb(root["locale"], root["name"])
+    except KeyError as e:
+        logging.warning(e)
+        return False
+    destination_file = os.path.join(destination_path, xkb_name + "-extended-mobile.json")
+
+    try:
+        with open(destination_file, 'x', encoding="utf-8") as dest_fd:
+            json.dump(mobile_terminal_root, dest_fd, ensure_ascii=False, indent=2, sort_keys=True)
     except FileExistsError as e:
         logging.info("File %s exists, not updating", destination_file)
         return False
